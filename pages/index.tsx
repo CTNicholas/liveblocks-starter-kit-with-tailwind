cbf06a7f58525d032b252ecfee5d74e8fa846eb6
CTNicholas <33033422+CTNicholas@users.noreply.github.com>
2023-04-25 14:38:32 +0100

    feat: Added Tailwind

--- pages/index.tsx
+++ pages/index.tsx
@@ -27,10 +27,9 @@ function Feature({ title, description, className, ...props }: FeatureProps) {
 export default function Index() {
   return (
     <MarketingLayout>
-    
-    {/* Testing Tailwind */}
+      {/* Testing Tailwind */}
       <div className="text-red-500 font-bold text-4xl">hello</div>
-      
+
       <Container className={styles.section}>
         <div className={styles.heroInfo}>
           <h1 className={styles.heroTitle}>
